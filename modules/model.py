dfbee4bfe271727e985e2d7b6312b8bbd10e821f
Mufti129 <ukhtytrader01@gmail.com>
2026-04-28 15:55:50 +0700

    model.py

--- modules/model.py
+++ modules/model.py
@@ -3,6 +3,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+import numpy as np
 
 def train_model(df, model_choice):
 
@@ -12,11 +13,14 @@ def train_model(df, model_choice):
         'dekat_fasilitas_transportasi_publik','jumlah_pasar_tradisional',
         'jumlah_restoran','jumlah_kompetitor','dekat_layanan_keuangan',
         'jumlah_bangunan','kategori_wilayah','jalan','jarak_pasar',
-        'kepadatan','kompetitor_ratio'
+        'kepadatan','kompetitor_ratio','fasilitas_total',
+        'lat','lon'
     ]
 
     X = df[features]
-    y = df['avg_omzet']
+
+    # 🔥 LOG TRANSFORM (WAJIB)
+    y = np.log1p(df['avg_omzet'])
 
     categorical = ['kategori_wilayah','jalan']
     numeric = [f for f in features if f not in categorical]
@@ -27,7 +31,14 @@ def train_model(df, model_choice):
     ])
 
     if model_choice == "Random Forest":
-        model = RandomForestRegressor(n_estimators=200, random_state=42)
+        model = RandomForestRegressor(
+            n_estimators=200,
+            max_depth=8,              # 🔥 diturunin biar ga overfit
+            min_samples_split=10,
+            min_samples_leaf=5,
+            max_features='sqrt',
+            random_state=42
+        )
     else:
         model = LinearRegression()
 
@@ -38,11 +49,18 @@ def train_model(df, model_choice):
 
     pipeline.fit(X, y)
 
-    return pipeline, features, preprocessor
+    return pipeline, features
 
 
+# ==========================================
+# PREDICT
+# ==========================================
 def predict(model, input_df):
+
     input_df['kepadatan'] = input_df['penduduk'] / (input_df['jumlah_bangunan'] + 1)
     input_df['kompetitor_ratio'] = input_df['jumlah_kompetitor'] / (input_df['jumlah_bangunan'] + 1)
+    input_df['fasilitas_total'] = input_df['jumlah_fasilitas_belanja'] + input_df['jumlah_restoran']
 
-    return model.predict(input_df)
+    pred_log = model.predict(input_df)
+
+    return np.expm1(pred_log)
