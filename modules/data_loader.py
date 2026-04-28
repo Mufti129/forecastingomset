b71b391be4def43252fe186dbed35cdae382b048
Mufti129 <ukhtytrader01@gmail.com>
2026-04-28 14:47:50 +0700

    data_loader.py

--- modules/data_loader.py
+++ modules/data_loader.py
@@ -6,7 +6,13 @@ def load_data():
 
     df['jumlah_bangunan'] = df['jumlah_bangunan'].astype(str).str.replace(',', '').astype(float)
 
+    # feature engineering
     df['kepadatan'] = df['penduduk'] / (df['jumlah_bangunan'] + 1)
     df['kompetitor_ratio'] = df['jumlah_kompetitor'] / (df['jumlah_bangunan'] + 1)
+
+    # lokasi
+    df = df.dropna(subset=['lat', 'lon'])
+    df['lat'] = df['lat'].astype(float)
+    df['lon'] = df['lon'].astype(float)
 
     return df
